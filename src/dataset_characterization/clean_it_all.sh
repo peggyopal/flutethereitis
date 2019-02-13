0d2a584b2ffdafcbb06453a1c5299dadc7a9ab77
kyles22 <25314187+KyleS22@users.noreply.github.com>
2019-02-13 09:25:13 -0600

    Updated script

--- src/dataset_characterization/clean_it_all.sh
+++ src/dataset_characterization/clean_it_all.sh
@@ -1,11 +1,56 @@
 #!/bin/sh
 
+echo "================================="
+echo "Cleaning Labels"
 python clean_labels.py
+echo "\nDone."
+echo "\n\n"
+
+echo "================================="
+echo "Cleaning Balanced Segments"
 
 python clean_segment.py ../../data/balanced_train_segments.csv
+echo "\nDone."
+echo "\n\n"
+
+
+echo "================================="
+echo "Cleaning Eval Segments"
+
 python clean_segment.py ../../data/eval_segments.csv
+echo "\nDone."
+echo "\n\n"
+
+
+echo "================================="
+echo "Cleaning Unbalanced Segments "
+
 python clean_segment.py ../../data/unbalanced_train_segments.csv
+echo "\nGroovy!"
+echo "\n\n"
+
+
+echo "================================="
+echo "Cleaning Balanced Tensors"
 
 python clean_embeddings.py ../../data/audioset_v1_embeddings/bal_train ../../data/balanced_train_segments_clean.csv
+echo "\nThat was tense!"
+echo "\n\n"
+
+
+echo "================================="
+echo "Cleaning Unbalanced Tensors"
+
 python clean_embeddings.py ../../data/audioset_v1_embeddings/unbal_train ../../data/unbalanced_train_segments_clean.csv
+echo "\nAlmost there..."
+echo "\n\n"
+
+
+echo "================================="
+echo "Cleaning Eval Tensors"
+
 python clean_embeddings.py ../../data/audioset_v1_embeddings/eval ../../data/eval_segments_clean.csv
+echo "\nYAY! ALL DONE!."
+echo "\n\n"
+
+
