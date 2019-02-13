cd95146343f1e9727789301488cb26a268759dff
Peggy Anderson <peggy.anderson@usask.ca>
2019-02-13 16:28:03 -0600

    Make flute be the only label we want clean_labels

--- src/dataset_characterization/clean_labels.py
+++ src/dataset_characterization/clean_labels.py
@@ -1,7 +1,7 @@
 """
 File Name: clean_labels.py
 
-Authors: Peggy Anderson & Kyle Seidenthal 
+Authors: Peggy Anderson & Kyle Seidenthal
 
 Date: 12-02-2019
 
@@ -12,30 +12,31 @@ Description: A script to remove labels that are not related to musical instrumen
 """
 import pandas as pd
 import os
-from tqdm import tqdm 
+from tqdm import tqdm
 
 PATH_TO_DATA_FOLDER = "../../data"
+PATH_TO_CLEAN_DATA_FOLDER = "../../data/clean_data"
 
 LABELS_CSV =  os.path.join(PATH_TO_DATA_FOLDER, "class_labels_indices.csv")
 
 
-labels_to_keep = ["Musical", "Instrument", "Plucked", "String",  "Drum", "Singing","Bass", "Acoustic", "Hi-Hat",
-"Cymbal", "Bagpipes", "Digeridoo", "Cello", "Flute", "Glockenspiel", "Clarinet", "Organ", "Percussion", "Trombone",
-"Banjo", "Mandolin", "Guitar", "Strum", "Harp", "Clapping", "Piano", "Trumpet", "Cowbell", "Harmonica", "Saxophone",
-"French Horn", "Theremin", "Timpani", "Rattle", "Jingle Bell", "Zither", "Rimshot","Harpsichord", "Maraca", "Yodeling",
-"Tubular Bells", "Gong", "Violin", "Fiddle"]
+# labels_to_keep = ["Musical", "Instrument", "Plucked", "String",  "Drum", "Singing","Bass", "Acoustic", "Hi-Hat",
+# "Cymbal", "Bagpipes", "Digeridoo", "Cello", "Flute", "Glockenspiel", "Clarinet", "Organ", "Percussion", "Trombone",
+# "Banjo", "Mandolin", "Guitar", "Strum", "Harp", "Clapping", "Piano", "Trumpet", "Cowbell", "Harmonica", "Saxophone",
+# "French Horn", "Theremin", "Timpani", "Rattle", "Jingle Bell", "Zither", "Rimshot","Harpsichord", "Maraca", "Yodeling",
+# "Tubular Bells", "Gong", "Violin", "Fiddle"]
+
+labels_to_keep = ["flute"]
 
 labels_df = pd.read_csv(LABELS_CSV)
-   
+
 clean_labels = pd.DataFrame()
-     
+
 for label in tqdm(labels_to_keep):
     hits = labels_df[labels_df["display_name"].str.lower().str.contains(label.lower())]
 
     clean_labels = clean_labels.append(hits)
 
 clean_labels = clean_labels.drop_duplicates(subset="display_name")
-  
-clean_labels.to_csv(os.path.join(PATH_TO_DATA_FOLDER, "clean_labels.csv"))
-    
 
+clean_labels.to_csv(os.path.join(PATH_TO_CLEAN_DATA_FOLDER, "class_labels_indicies_cleaned.csv"))
