cd98674a851ac6644f7f712aeff31cd423173470
kyles22 <25314187+KyleS22@users.noreply.github.com>
2019-02-14 08:59:33 -0600

    Update clean it all to match file name structure

--- src/dataset_characterization/clean_it_all.sh
+++ src/dataset_characterization/clean_it_all.sh
@@ -33,7 +33,7 @@ echo "\n\n"
 echo "================================="
 echo "Cleaning Balanced Tensors"
 
-python clean_embeddings.py ../../data/bal_train ../../data/clean_data/balanced_train_segments_cleaned.csv flute
+python clean_embeddings.py ../../data/bal_train ../../data/clean_data/balanced_train_segments_cleaned.csv flute_didgeridoo
 echo "\nThat was tense!"
 echo "\n\n"
 
@@ -41,7 +41,7 @@ echo "\n\n"
 echo "================================="
 echo "Cleaning Unbalanced Tensors"
 
-python clean_embeddings.py ../../data/unbal_train ../../data/clean_data/unbalanced_train_segments_cleaned.csv flute
+python clean_embeddings.py ../../data/unbal_train ../../data/clean_data/unbalanced_train_segments_cleaned.csv flute_didgeridoo
 echo "\nAlmost there..."
 echo "\n\n"
 
@@ -49,7 +49,8 @@ echo "\n\n"
 echo "================================="
 echo "Cleaning Eval Tensors"
 
-python clean_embeddings.py ../../data/eval ../../data/clean_data/eval_segments_cleaned.csv flute
+python clean_embeddings.py ../../data/eval ../../data/clean_data/eval_segments_cleaned.csv flute_didgeridoo
+
 echo "\nYAY! ALL DONE!."
 echo "\n\n"
 
