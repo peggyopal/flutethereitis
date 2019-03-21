9a719980e47ac977f55c9635de0a139e496ef40c
Peggy Anderson <peggy.anderson@usask.ca>
2019-03-21 12:00:28 -0600

    Remove unnecessary helpers import

--- src/dataset_characterization/clean_labels.py
+++ src/dataset_characterization/clean_labels.py
@@ -16,10 +16,6 @@ import os
 import pandas as pd
 from tqdm import tqdm
 
-module_path = os.path.dirname(os.path.abspath("src/helpers.py"))
-sys.path.insert(0, module_path + '/../')
-import src.helpers as help
-
 
 PATH_TO_DATA_FOLDER = os.path.abspath("data/")
 PATH_TO_CLEAN_DATA_FOLDER = os.path.abspath("data/clean_data")
@@ -35,17 +31,4 @@ def clean_all_labels():
 # "French Horn", "Theremin", "Timpani", "Rattle", "Jingle Bell", "Zither", "Rimshot","Harpsichord", "Maraca", "Yodeling",
 # "Tubular Bells", "Gong", "Violin", "Fiddle"]
 
-    labels_to_keep = ["flute", "didgeridoo"]
-
-    labels_df = pd.read_csv(LABELS_CSV)
-
-    clean_labels = pd.DataFrame()
-
-    for label in tqdm(labels_to_keep):
-        hits = labels_df[labels_df["display_name"].str.lower().str.contains(label.lower())]
-
-        clean_labels = clean_labels.append(hits)
-
-    clean_labels = clean_labels.drop_duplicates(subset="display_name")
-
-    clean_labels.to_csv(os.path.join(PATH_TO_CLEAN_DATA_FOLDER, "class_labels_indicies_cleaned.csv"))
+aan_labels.to_csv(os.path.join(PATH_TO_CLEAN_DATA_FOLDER, "class_labels_indicies_cleaned.csv"))
