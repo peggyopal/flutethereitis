0399810679e89e85b443a915ebd725b3ef45c55c
poa681 <peggy.anderson@usask.ca>
2019-03-29 11:27:33 -0600

    Fix import and index in test

--- src/models/hidden_markov/test.py
+++ src/models/hidden_markov/test.py
@@ -14,8 +14,8 @@ import numpy as np
 
 from hmmlearn import hmm
 
-module_path = os.path.dirname(os.path.abspath("src/models/process_data.py"))
-sys.path.insert(0, module_path + '/../../')
+module_path = os.path.dirname(os.path.abspath("src/models"))
+sys.path.insert(0, module_path + '/../')
 from src.models.process_data import get_eval, get_bal_train, get_unbal_train
 
 import pandas as pd
@@ -33,10 +33,10 @@ def create_lists_of_audio_embeddings(dataset):
     for video_id_features in dataset:
         for video_id in video_id_features:
             if 'Flute' in video_id_features[video_id]['labels']:
-                flute_audio_embeddings.append(video_id_features[video_id]['audio_embeddings'])
+                flute_audio_embeddings.append(video_id_features[video_id]['audio_embedding'])
 
             if 'Didgeridoo' in video_id_features[video_id]['labels']:
-                didgerigoo_audio_embeddings.append(video_id_features[video_id]['audio_embeddings'])
+                didgerigoo_audio_embeddings.append(video_id_features[video_id]['audio_embedding'])
 
     return flute_audio_embeddings, didgerigoo_audio_embeddings
 
@@ -60,7 +60,7 @@ if __name__ == "__main__":
 
     print(len(didgerigoo_aes_bal))
     print(len(flute_aes_bal))
-    
+
     # X, Z = model.sample(100)
     #
     # print(Z)
