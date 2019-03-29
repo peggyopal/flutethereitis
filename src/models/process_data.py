2781e2967f11091a6290d4c797216d4de404f91a
poa681 <peggy.anderson@usask.ca>
2019-03-29 09:41:08 -0600

    Remove dirs being created when processing clean data

--- src/models/process_data.py
+++ src/models/process_data.py
@@ -10,21 +10,15 @@ Description: A file to process the data to prepare it for evaluation with the
 
 """
 
-
 import pandas as pd
 import matplotlib.pyplot as plt
 import multiprocessing as mp
-import tensorflow as tf
 
 import json
 import os
-import sys
+import tensorflow as tf
 import tqdm         # progress bar
 import unittest
-
-module_path = os.path.dirname(os.path.abspath("src/helpers.py"))
-sys.path.insert(0, module_path + '/../')
-import src.helpers as help
 
 
 # Pooling stuff
@@ -35,9 +29,57 @@ PATH_TO_DATA_FOLDER = os.path.abspath("data/")
 PATH_TO_CLEAN_DATA_FOLDER = os.path.abspath("data/clean_data")
 PATH_TO_UNBAL_TRAIN_FOLDER = PATH_TO_CLEAN_DATA_FOLDER + "/unbal_train/flute_didgeridoo"
 PATH_TO_BAL_TRAIN_FOLDER = PATH_TO_CLEAN_DATA_FOLDER + "/bal_train/flute_didgeridoo"
-# PATH_TO_EVAL_FOLDER = PATH_TO_DATA_FOLDER + "/eval"
 PATH_TO_EVAL_FOLDER = PATH_TO_CLEAN_DATA_FOLDER + "/eval/flute_didgeridoo"
 
+# Labels Stuff
+LABELS_CSV = os.path.join(PATH_TO_DATA_FOLDER, "class_labels_indices.csv")
+
+
+def _lookup_label_by_index(label_int):
+    """
+    Given a integer value for a label lookup the string value
+
+    :param label_int: A label as an integer value
+    :returns: String representation of a label if found, if index is not valid
+              the program will continue as normal, but a value in the list is
+              yelling at you
+    """
+    label = pd.read_csv(LABELS_CSV)
+    try:
+        return label["display_name"].loc[label["index"] == label_int].item()
+    except:
+        return "CAN'T FIND LABEL INDEX"
+
+
+def _extract_sequence(tf_data):
+    """
+    Extract the SequenceExample as a string from a given TensorFlow record
+
+    :param data_dir: A TensorFlow record
+    :returns: String value of SequenceExample of TensorFlow record
+    """
+    sequence = tf.train.SequenceExample()
+    sequence.ParseFromString(tf_data)
+    return sequence
+
+
+def _extract_audio_embedding(ae_features):
+    """
+    Extract Audio Embedding as a List
+
+    :param ae_features: A TensorFlow feature list
+    :returns: A list of the audio embeddings as float values
+    """
+    audio_embeddings = []
+
+    sess = tf.InteractiveSession()      # Need to start this for .eval()
+    for second in range(0, len(ae_features)):
+        raw_embedding = tf.decode_raw(ae_features[second].bytes_list.value[0],tf.uint8)
+        float_embedding = tf.cast(raw_embedding, tf.float32).eval().tolist()
+        audio_embeddings.append(float_embedding)
+    sess.close()
+
+    return audio_embeddings
 
 
 def _convert_labels(protobuf):
@@ -51,7 +93,7 @@ def _convert_labels(protobuf):
     """
     labels = []
     for i in range(0, len(protobuf)):
-        label_as_string = help.lookup_label_by_index(protobuf[i])
+        label_as_string = _lookup_label_by_index(protobuf[i])
         labels.append(label_as_string)
     return labels
 
@@ -70,18 +112,18 @@ def _process_tensor_file(tf_file_path):
     raw_data = tf.python_io.tf_record_iterator(path=tf_file_path)
 
     for record in raw_data:
-        sequence = help.extract_sequence(record)
+        sequence = _extract_sequence(record)
         video_id = sequence.context.feature["video_id"].bytes_list.value
 
         labels_protobuf = sequence.context.feature["labels"].int64_list.value
-        labels = help.convert_feature_key_to_string(labels_protobuf)
+        labels = _convert_labels(labels_protobuf)
 
         audio_embedding_features = sequence.feature_lists.feature_list["audio_embedding"].feature
-        audio_embedding_list = help.extract_audio_embedding(audio_embedding_features)
+        audio_embedding_list = _extract_audio_embedding(audio_embedding_features)
 
         data[video_id[0]] = {
                             "labels": labels,
-                            "audio_embeddings": audio_embedding_list
+                            "audio_embedding": audio_embedding_list
                         }
 
     return data
@@ -93,17 +135,10 @@ def _process_data(dir_path):
     :param data_dir: The path to the tensorflow records to process
     :returns:
     """
-
-    if not os.path.exists(dir_path):
-        os.makedirs(dir_path)
-
     data = {}
-    print(dir_path)
     all_tf_files = os.listdir(dir_path)
-    all_tf_files = [os.path.join(dir_path, x) for x in all_tf_files if not x.startswith(".")]
     pool = mp.Pool(NUM_WORKERS)
-    # print(len([os.path.join(dir_path, x) for x in all_tf_files]))
-    result = list(tqdm.tqdm(pool.imap(_process_tensor_file, all_tf_files), total=len(all_tf_files), unit="files"))
+    result = list(tqdm.tqdm(pool.imap(_process_tensor_file, [os.path.join(dir_path, x) for x in all_tf_files]), total=len(all_tf_files), unit="files"))
 
     return result
 
