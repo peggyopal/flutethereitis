e24b84676d8591cead62ebb1f7bf1eee37c9e478
poa681 <peggy.anderson@usask.ca>
2019-03-29 12:00:13 -0600

    Add check for training type

--- run.py
+++ run.py
@@ -12,17 +12,21 @@ Description: A file to run the classifier with HMM or RNN
 from src.models.hidden_markov import test
 
 
-def get_model_from_command_line():
-    allowed_models = ["RNN", "HMM"]
+if __name__ == "__main__":
     print("So you want to determine if there is a flute?")
-    model = str(input("Enter either HMM or RNN: ")).upper()
+    allowed_models = ["RNN", "HMM"]
+    model = str(input("Enter model (either HMM or RNN): ")).upper()
     while model not in allowed_models:
         model = str(input("Enter either HMM or RNN: ")).upper()
-    return model
 
+    if model == 'RNN':
+        print("There is another way to run RNN, please refer to the documentation")
+        exit()
 
-if __name__ == "__main__":
-    model = get_model_from_command_line()
+    allowed_trains = ["bal", "unbal"]
+    training_set = str(input("Enter training set (either bal or unbal): ")).lower()
+    while training_set not in allowed_trains:
+        training_set = str(input("Enter training set (either bal or unbal): ")).lower()
 
     if model == 'HMM':
         print("Running HMM Classifier.... \n")
