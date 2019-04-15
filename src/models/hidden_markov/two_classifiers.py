4e6301e0cf3073c2f96b471510017c7225df29cb
poa681 <peggy.anderson@usask.ca>
2019-04-15 12:06:53 -0600

    Fix classifier

--- src/models/hidden_markov/two_classifiers.py
+++ src/models/hidden_markov/two_classifiers.py
@@ -14,10 +14,11 @@ import os
 
 
 from hmmlearn import hmm
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, KFold
 
 import pandas as pd
 import matplotlib.pyplot as plt
+import random
 import numpy as np
 import warnings
 
@@ -26,15 +27,20 @@ import warnings
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 
+def _check_ae_array_length(audio_embedding):
+    while len(audio_embedding) != 10:
+        audio_embedding.append(np.zeros(128))
+    return audio_embedding
+
+
 def _create_arrays_of_audio_embeddings(dataset):
     flute_audio_embeddings = []
     didgerigoo_audio_embeddings = []
 
     for video_id_features in dataset:
         for video_id in video_id_features:
-            audio_embeddings = sum(video_id_features[video_id]['audio_embedding'], [])
-            if len(audio_embeddings) != 1280:
-                audio_embeddings = audio_embeddings + ([0] * (1280 - len(audio_embeddings)))
+            audio_embeddings = video_id_features[video_id]['audio_embedding']
+            _check_ae_array_length(audio_embeddings)
 
             if 'Flute' in video_id_features[video_id]['labels']:
                 flute_audio_embeddings.append(audio_embeddings)
@@ -49,52 +55,90 @@ def _create_arrays_of_audio_embeddings(dataset):
 
 
 def _fit_classifier(training_set):
-    model = hmm.GaussianHMM(n_components=2, algorithm='map')
+    model = hmm.GaussianHMM(n_components=2, algorithm='viterbi')
+    # model = hmm.GMMHMM(n_components=2, init_params="st")
     with warnings.catch_warnings():
         warnings.filterwarnings('ignore', category=DeprecationWarning)
-        model.fit(training_set)
+        for audio_embedding in training_set:
+            model.fit(audio_embedding)
 
     return model
 
 
+def _classify(dataset, fHMM, dHMM):
+    true_positive = 0
+    false_negative = 0
+    true_negative = 0
+    false_positive = 0
+    idk = 0
+
+    count = 0
+
+    for video_id_features in dataset:
+        for video_id in video_id_features:
+            audio_embedding = video_id_features[video_id]['audio_embedding']
+            _check_ae_array_length(audio_embedding)
+
+            label = video_id_features[video_id]['labels']
+
+            # Check if there is more than one label associated with the data
+            if len(label) > 1:
+                for l in label:
+                    if l == 'Flute':
+                        label = 'Flute'
+                        break
+
+            flute_proba = fHMM.predict(audio_embedding)
+            didgeridoo_proba = dHMM.predict(audio_embedding)
+
+            if sum(flute_proba) > sum(didgeridoo_proba):
+                if label[0] == 'Flute':
+                    true_positive += 1
+                else:
+                    false_negative += 1
+            elif sum(flute_proba) < sum(didgeridoo_proba):
+                if label[0] == 'Didgeridoo':
+                    true_negative += 1
+                else:
+                    false_positive += 1
+            else:
+                idk += 1
+
+    print(count)
+    return (true_positive, false_negative, true_negative, false_positive, idk)
+
+
+def _compute_precision_recall_accuracy(classifications):
+    true_positive = classifications[0]
+    false_negative = classifications[1]
+    true_negative = classifications[2]
+    false_positive = classifications[3]
+
+    precision = true_positive / (true_positive + false_positive)
+    recall = true_positive / (true_positive + false_negative)
+    accuracy = true_positive / (true_negative + false_negative + false_positive + true_positive)
+
+    return precision, recall, accuracy
+
+
 def hmm_run(train_data, eval_data):
-    # print("Get Datasets... ")
-    # train_data, eval_data = get_datasets(training_set)
-    # print("Datasets Got! \n")
-
-    flute_aes, didgeridoo_aes = _create_arrays_of_audio_embeddings(train_data)
-    flute_eval, didgeridoo_eval = _create_arrays_of_audio_embeddings(eval_data)
-
-    flute_train, flute_test = train_test_split(flute_aes)
-    didgeridoo_train, didgeridoo_test = train_test_split(didgeridoo_aes)
-
-    print("train: ", len(didgeridoo_train))
-    print("test: ", len(didgeridoo_test))
-    print(len(didgeridoo_aes))
+    flute_train, didgeridoo_train = _create_arrays_of_audio_embeddings(train_data)
 
     flute_hmm = _fit_classifier(flute_train)
     didgeridoo_hmm = _fit_classifier(didgeridoo_train)
 
-    predictions = flute_hmm.predict_proba(flute_test)
-    print("predictions: ", predictions)
+    classifications = _classify(eval_data, flute_hmm, didgeridoo_hmm)
+    # test_precision, test_recall, test_accuracy = _compute_precision_recall_accuracy(classifications)
 
-    predictions = didgeridoo_hmm.predict_proba(didgeridoo_test)
-    print("predictions: ", predictions)
+    print("classifications: {}".format(classifications))
+    # print("precision: {}".format(test_precision))
+    # print("recall: {}".format(test_recall))
+    # print("accuracy: {}".format(test_accuracy))
+    ## use hmm.predict_proba(X)
 
-    score = flute_hmm.score(flute_eval)
-    print("flute_hmm flute_eval score: ", score)
-    score = flute_hmm.score(didgeridoo_eval)
-    print("flute_hmm didgeridoo_eval score: ", score)
-
-    score = didgeridoo_hmm.score(flute_eval)
-    print("didgeridoo_hmm flute_eval score: ", score)
-    score = didgeridoo_hmm.score(didgeridoo_eval)
-    print("didgeridoo_hmm didgeridoo_eval score: ", score)
-    # # use hmm.predict_proba(X)
-
-# import json
-# TRAIN_JSON = "data/clean_data/train_data_json.txt"
-# EVAL_JSON = "data/clean_data/eval_data_json.txt"
-# training = json.load(open(TRAIN_JSON))
-# eval = json.load(open(EVAL_JSON))
-# hmm_run(training, eval)
+import json
+TRAIN_JSON = "data/clean_data/train_data_json.txt"
+EVAL_JSON = "data/clean_data/eval_data_json.txt"
+training = json.load(open(TRAIN_JSON))
+eval = json.load(open(EVAL_JSON))
+hmm_run(training, eval)
